8b03e9ffc740848b16a6544c6807bab1bfef63d7
pml68 <tutorialmester@gmail.com>
2024-02-02 16:51:59 +0100

    feat: circle area calculator

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main() {
-  srand(time(0));
-  printf("%d\n", rand());
+  const double PI = 3.141592;
+  double r;
+  double a;
+
+  printf("\n Enter the radius of a circle: ");
+  scanf("%lf", &r);
+
+  a = r * r * PI;
+
+  printf("\nArea: %lf", a);
   return 0;
 }
